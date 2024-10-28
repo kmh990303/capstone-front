44ec2f462fa45087445dd0c651297e5694fd391d
Minhyuk Kim <labcs@naver.com>
2024-10-28 18:23:34 +0900

    feat: 네비게이션 바 구축

---
+++ components/NavBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+
+import NavLogo from "@/images/navLogo.png";
+
+import Image from "next/image";
+
+import { useRouter } from "next/navigation";
+
+const NavBar: React.FC = () => {
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.push("/");
+  };
+
+  return (
+    <menu className="w-full h-[10vh] bg-white flex items-center">
+      <Image
+        src={NavLogo}
+        alt="NavLogo"
+        className="ml-10 cursor-pointer"
+        onClick={handleLogoClick}
+      />
+    </menu>
+  );
+};
+
+export default NavBar;
