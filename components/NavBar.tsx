599641eb723a33d05d0484b3500233eca1a3f74d
Minhyuk Kim <labcs@naver.com>
2024-11-27 14:32:40 +0900

    feat: 메인 분석 이동 버튼 구현

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,10 +9,12 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
 import { useAuthStore } from "@/lib/store";
+import { useAreaStore } from "@/lib/store";
 
 const NavBar: React.FC = () => {
   const router = useRouter();
   const { loginSuccess, setLoginSuccess } = useAuthStore();
+  const { name } = useAreaStore();
 
   const handleLogoClick = () => {
     router.push("/");
@@ -24,6 +26,10 @@ const NavBar: React.FC = () => {
     router.push("/");
   };
 
+  const handleMainAnalysis = () => {
+    router.push("/marketAreaAnalysis");
+  };
+
   return (
     <menu className="w-full h-[10vh] bg-white flex items-center border-b-2 border-gray-100 justify-between">
       <Image
@@ -32,20 +38,35 @@ const NavBar: React.FC = () => {
         className="ml-10 cursor-pointer w-[12rem]"
         onClick={handleLogoClick}
       />
-      {/* 액세스 토큰이 있을 때만 보이게 설정 */}
-      {loginSuccess && (
-        <>
-          <motion.button
-            className="areaAnalysis10 px-6 py-2 mr-12 rounded-md border-2 border-violet-400"
-            style={{ backgroundColor: "#FFFFFF" }}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.95 }}
-            onClick={handleLogout}
-          >
-            로그아웃
-          </motion.button>
-        </>
-      )}
+      <div>
+        {name && (
+          <>
+            <motion.button
+              className="areaAnalysis10 px-6 py-2 mr-8 rounded-md border-2 border-violet-400"
+              style={{ backgroundColor: "#FFFFFF" }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={handleMainAnalysis}
+            >
+              메인 분석
+            </motion.button>
+          </>
+        )}
+        {/* 액세스 토큰이 있을 때만 보이게 설정 */}
+        {loginSuccess && (
+          <>
+            <motion.button
+              className="areaAnalysis10 px-6 py-2 mr-12 rounded-md border-2 border-violet-400"
+              style={{ backgroundColor: "#FFFFFF" }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={handleLogout}
+            >
+              로그아웃
+            </motion.button>
+          </>
+        )}
+      </div>
     </menu>
   );
 };
