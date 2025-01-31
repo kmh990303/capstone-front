665e3a98c174b2d323cef30a606bfb56a341a897
kmh990303 <labcs@naver.com>
2025-01-31 06:27:59 +0000

    test: 로그인 테스트 코드 3차 작성

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,31 +1,31 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('로그인 테스트', () => {
-    test('올바른 이메일과 패스워드 입력', async ({ page }) => {
+    test("올바른 아이디와 비밀번호 입력", async ({ page }) => {
         await page.goto('/');
 
-        await page.fill('input[name="email"]', 'mmm@gmail.com');
+        await page.fill('input[name="email"]', "mmm@gmail.com");
 
-        await page.fill('input[name="password"]', '11111111');
+        await page.fill('input[name="password"]', "11111111");
 
         await page.click('button[type="submit"]');
 
         await expect(page).toHaveURL('/inputArea', { timeout: 10000 });
     })
 
-    test('로그인 실패 - 잘못된 이메일 또는 비밀번호', async ({ page }) => {
+    test("잘못된 아이디와 비밀번호 입력", async ({ page }) => {
         await page.goto('/');
 
         await page.fill('input[name="email"]', "wrong@example.com");
 
-        await page.fill('input[name="password"]', "22222222");
+        await page.fill('input[name="password"]', "222222222");
 
         await page.click('button[type="submit"]');
 
         page.on("dialog", async (dialog) => {
             expect(dialog.message()).toBe("아이디와 비밀번호를 다시 확인해주세요.");
             await dialog.dismiss();
-        });
+        })
 
         await expect(page).toHaveURL('/', { timeout: 10000 });
     })
